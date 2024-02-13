d49f066420b74bb4a961e3e69ec7c6c4912717ff
JonHowMIT <52931333+JonHowMIT@users.noreply.github.com>
2024-02-12 20:04:12 -0500

    ff

--- read_csv_data.py
+++ read_csv_data.py
@@ -1,1 +1,23 @@
-read_csv_data.py
+import csv
+import numpy as np
+
+def Read_csv_data(filename, Ngrab = 3):
+    data_grab = []
+    dc = np.zeros(Ngrab)
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=",")   
+        for row in reader:
+            if (row[0][0] != '#'): # ignore comments
+                for ii in range(Ngrab):
+                    dc[ii] = float(row[ii])
+                if (len(data_grab) == 0):
+                    data_grab = dc
+                else:
+                    data_grab = np.r_['0,2,1', data_grab, dc]
+            else:
+                try:
+                    print(row[0], end =" ")
+                    print ('%s' % ', '.join(map(str, row[1:])))
+                except:
+                    print(" ")
+    return data_grab
